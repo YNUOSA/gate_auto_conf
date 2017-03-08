c7dabf3153b4c6a5f8858049f6ffd3c44d952b68
STRRL <str_ruiling@outlook.com>
2017-03-08 14:19:26 +0800

    finish

--- index/index.py
+++ index/index.py
@@ -1,8 +1,33 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField, validators, DecimalField, HiddenField
+
 import logging, json
 
+
+class nginxForm(FlaskForm):
+    id = HiddenField('id')
+    domain = StringField('domain', [validators.DataRequired()])
+    listen_port = IntegerField('listen_port', [validators.DataRequired(), validators.NumberRange(min=0, max=65535)])
+    dst = StringField('dst', [validators.DataRequired()])
+    remark = StringField('remark', [validators.DataRequired()])
+    submit = SubmitField('Save')
+
+
+class rinetdForm(FlaskForm):
+    id = HiddenField('id')
+    bindaddress = StringField('bindaddress', [validators.DataRequired(), validators.IPAddress()])
+    bindport = IntegerField('bindport', [validators.DataRequired(), validators.NumberRange(min=0, max=65535)])
+    connectaddress = StringField('connectaddress', [validators.DataRequired(), validators.IPAddress()])
+    connectport = IntegerField('connectport', [validators.DataRequired(), validators.NumberRange(min=0, max=65535)])
+    submit = SubmitField('Save')
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'what a testing project'
+Bootstrap(app)
 
 
 def sortbyKey(array, _key):
@@ -75,7 +100,7 @@ def index():
     nv = []
     nv.append({'href': '/nginx-list', 'caption': 'View nginx Rules'})
     nv.append({'href': '/rinetd-list', 'caption': 'View rinetd Rules'})
-    return render_template('index.html', a_variable='a', navigation=nv)
+    return render_template('index.html', navigation=nv)
 
 
 @app.route('/nginx-list')
@@ -87,11 +112,15 @@ def nginx_list():
 @app.route('/nginx-edit', methods=['GET'])
 def nginx_edit():
     jo = sortbyKey(json.load(open('../nginx.json')), 'domain')
+    ngf = nginxForm()
     for item in jo:
         if str(item['id']) == request.args.get('id'):
-            return render_template('nginx-edit.html', id=item['id'], domain=item['domain'],
-                                   listen_port=item['listen_port'], dst=item['dst'], remark=item['remark'],
-                                   edit=item['domain'])
+            ngf.id.data = item['id']
+            ngf.domain.data = item['domain']
+            ngf.listen_port.data = item['listen_port']
+            ngf.dst.data = item['dst']
+            ngf.remark.data = item['remark']
+            return render_template('nginx-edit.html', form=ngf)
         else:
             continue
     return render_template('nginx-edit.html', add=request.args.get('domain'))
@@ -99,7 +128,8 @@ def nginx_edit():
 
 @app.route('/nginx-add')
 def nginx_add():
-    return render_template('nginx-edit.html', add='new rule')
+    form = nginxForm()
+    return render_template('nginx-edit.html', form=form)
 
 
 @app.route('/nginx-delete', methods=['GET', 'POST'])
@@ -127,9 +157,15 @@ def nginx_delete():
 @app.route('/nginx-save', methods=['GET', 'POST'])
 def nginx_save():
     if request.method == 'POST':
-        # doamin listen_port dst remark]
-        return save_nginx_conf(request.form['id'], request.form['domain'], request.form['listen_port'],
-                                request.form['dst'], request.form['remark'])
+        form = nginxForm()
+        if form.validate_on_submit():
+            return save_nginx_conf(request.form['id'], request.form['domain'], request.form['listen_port'],
+                                   request.form['dst'], request.form['remark'])
+        else:
+            return render_template('succeed.html', title='faild', msg='argument error', msgdetail=form.errors)
+            # doamin listen_port dst remark]
+            # return save_nginx_conf(request.form['id'], request.form['domain'], request.form['listen_port'],
+            #                        request.form['dst'], request.form['remark'])
 
 
 @app.route('/rinetd-list')
@@ -143,10 +179,13 @@ def rinetd_edit():
     jo = sortbyKey(json.load(open('../rinetd.json')), 'connectaddress')
     for item in jo:
         if str(item['id']) == request.args.get('id'):
-            return render_template('rinetd-edit.html', bindaddress=item['bindaddress'], bindport=item['bindport'],
-                                   connectaddress=item['connectaddress'], connectport=item['connectport'],
-                                   remark=item['remark'], id=item['id'], edit='%s:%s => %s:%s' % (
-                    item['bindaddress'], item['bindport'], item['connectaddress'], item['connectport']))
+            form = rinetdForm()
+            form.id.data = item['id']
+            form.bindaddress.data = item['bindaddress']
+            form.bindport.data = item['bindport']
+            form.connectaddress.data = item['connectaddress']
+            form.connectport.data = item['connectport']
+            return render_template('rinetd-edit.html', form=form)
     return render_template('rinetd-edit.html', add='new rule')
 
 
@@ -180,13 +219,27 @@ def rinetd_delete():
 @app.route('/rinetd-save', methods=['GET', 'POST'])
 def rinetd_save():
     if request.method == 'POST':
-        return save_rinetd_conf(request.form['id'], request.form['bindaddress'], request.form['bindport'],
-                                request.form['connectaddress'], request.form['connectport'])
+        form = rinetdForm()
+        if form.validate_on_submit():
+            return save_rinetd_conf(request.form['id'], request.form['bindaddress'], request.form['bindport'],
+                                    request.form['connectaddress'], request.form['connectport'])
+        else:
+            return render_template('succeed.html', title='faild', msg='argument error', msgdetail=form.errors)
 
 
 @app.route('/log')
 def log():
     return open('../auto_conf.log').read().replace('\n', '<br>')
+
+
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return render_template('404.html'), 404
+#
+#
+# @app.errorhandler(500)
+# def internal_server_error(e):
+#     return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
